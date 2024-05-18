0ba3f8cb2abc2246ba5073e0dc3bee7d29bbcf9d
Data <DataCpt@users.noreply.github.com>
2024-05-19 00:41:35 +0100

    Formatting

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,9 @@
 local turret = data.raw["fluid-turret"]["flamethrower-turret"]
 
-if settings.startup["ftb_consumption_multiplier"].value and turret then
-    -- Default vanilla consumption: 0.2
-    turret.attack_parameters.fluid_consumption = 0.2 * settings.startup["ftb_consumption_multiplier"].value
-    log("Flamethrower turret consumption increased to: " .. turret.attack_parameters.fluid_consumption)
+local consumption_multiplier = settings.startup["ftb_consumption_multiplier"].value
+
+if consumption_multiplier and turret then
+  -- Default vanilla consumption: 0.2
+  turret.attack_parameters.fluid_consumption = 0.2 * consumption_multiplier
+  log("Flamethrower turret consumption increased to: " .. turret.attack_parameters.fluid_consumption)
 end
--- settings.lua
+++ settings.lua
@@ -1,11 +1,9 @@
-data:extend({    
-    {
-        type = "double-setting",
-        name = "ftb_consumption_multiplier",
-        setting_type = "startup",
-        default_value = 4.0,
-        minimum_value = 0.1,
-        maximum_value = 100.0,
-        order = "a"
-    }
-})
+data:extend({{
+  type = "double-setting",
+  name = "ftb_consumption_multiplier",
+  setting_type = "startup",
+  default_value = 4.0,
+  minimum_value = 0.1,
+  maximum_value = 100.0,
+  order = "a"
+}})
