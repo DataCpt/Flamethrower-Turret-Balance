4fdb1da6de8f0bba2658a2efcbfc894d58c8ffe6
Data <DataCpt@users.noreply.github.com>
2025-09-30 10:47:37 +0100

    Added conditional data-util usage

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,9 +1,19 @@
 require("data.settings")
+if mods["data-utils"] then require("__data-utils__.data.util") end
 
-local turret = data.raw["fluid-turret"]["flamethrower-turret"]
+local function update_turret(turret)
+  if not turret or not turret.attack_parameters or not turret.attack_parameters.fluid_consumption then return end
+  if mods["data-utils"] then
+    adjust_attr(turret, "attack_parameters.fluid_consumption", FTB_CONSUMPTION_MULTIPLIER)
+  else
+    turret.attack_parameters.fluid_consumption = turret.attack_parameters.fluid_consumption * FTB_CONSUMPTION_MULTIPLIER
+    if DU_DEBUG then log("Flamethrower turret consumption increased to " .. turret.attack_parameters.fluid_consumption) end
+  end
+end
 
-if turret then
+if not FTB_ALL_TURRETS then
   -- Default vanilla consumption: 0.2
-  turret.attack_parameters.fluid_consumption = 0.2 * FTB_CONSUMPTION_MULTIPLIER
-  debug_log("Flamethrower turret consumption increased to " .. turret.attack_parameters.fluid_consumption)
+  update_turret(data.raw["fluid-turret"]["flamethrower-turret"])
+else
+  for _, turret in pairs(data.raw["fluid-turret"]) do update_turret(turret) end
 end
--- data/settings.lua
+++ data/settings.lua
@@ -1,3 +1,17 @@
-require("data.util")
+if mods["data-utils"] then require("__data-utils__.data.util") end
 
-FTB_CONSUMPTION_MULTIPLIER = setup_setting("ftb_consumption_multiplier", 4.0)
+-- DU_DEBUG = true
+
+-- from "__data-utils__.data.util"
+local setting = mods["data-utils"] and setup_setting or function(name, default)
+  if settings.startup[name] then
+    if DU_DEBUG then log("Setting " .. name .. " to " .. tostring(settings.startup[name].value)) end
+    return settings.startup[name].value
+  else
+    if DU_DEBUG then log("Setting: " .. name .. " not found; using default: " .. tostring(default)) end
+    return default
+  end
+end
+
+FTB_CONSUMPTION_MULTIPLIER = setting("ftb_consumption_multiplier", 4.0)
+FTB_ALL_TURRETS = setting("ftb_all_turrets", false)
