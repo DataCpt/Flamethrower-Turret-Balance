3cd7371f6ee44d60b71c00e33dd7a2af811faeee
Data <DataCpt@users.noreply.github.com>
2025-09-30 10:18:22 +0100

    Adjusted setting range

--- settings.lua
+++ settings.lua
@@ -1,9 +1,9 @@
-data:extend({{
+data:extend {{
   type = "double-setting",
   name = "ftb_consumption_multiplier",
   setting_type = "startup",
   default_value = 4.0,
-  minimum_value = 0.01,
-  maximum_value = 1000.0,
-  order = "a"
-}})
+  minimum_value = 1e-10,
+  maximum_value = 1e+10,
+  order = "a",
+}}
